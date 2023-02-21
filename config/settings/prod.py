75b568b4d0452a9f1a43f81af13be0c870154ece
Sreeniketh Madgula <sreeniketh.madgula@gmail.com>
2023-02-22 02:14:56 +0530

    added railway postgres db

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,14 @@
 from .base import *
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'containers-us-west-76.railway.app',
+        'NAME': 'elevators_test',
+        'USER': 'postgres',
+        'PASSWORD': 'yx34bwYPjz5VTTAc14nr',
+        'PORT': 6137
+    }
+}
