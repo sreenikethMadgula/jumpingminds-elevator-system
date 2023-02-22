fb59f16fa3833997be5f5ba24a6058d22d029be6
Sreeniketh Madgula <sreeniketh.madgula@gmail.com>
2023-02-22 05:50:39 +0530

    changed configurations for render

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -6,18 +6,18 @@ DEBUG = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'elevators_test',
-        'USER': 'user1',
-        'PASSWORD': 'password'
-    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'HOST': 'containers-us-west-76.railway.app',
-    #     'NAME': 'railway',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'yx34bwYPjz5VTTAc14nr',
-    #     'PORT': 6137
+    #     'NAME': 'elevators_test',
+    #     'USER': 'user1',
+    #     'PASSWORD': 'password'
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'containers-us-west-76.railway.app',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'yx34bwYPjz5VTTAc14nr',
+        'PORT': 6137
+    }
 }
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+# gunicorn config.wsgi:application --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
